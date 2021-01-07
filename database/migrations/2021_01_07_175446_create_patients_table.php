8268eb1a670c462a5c3f2eb984b9950404318110
nihel-7 <58957635+nihel-7@users.noreply.github.com>
2021-01-08 00:33:25 +0100

    database migrations

--- database/migrations/2021_01_07_175446_create_patients_table.php
+++ database/migrations/2021_01_07_175446_create_patients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePatientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('patients', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('specialite')->nullable();
-            $table->integer('role');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('npatient');
+            $table->date('patient_birth_date');
+            $table->string('phone');
+            $table->mediumText('maladies');
+            $table->mediumText('allergies');
+            $table->mediumText('antecedents');
+            $table->mediumText('commentaires');
+
             $table->timestamps();
         });
     }
@@ -33,6 +35,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('patients');
     }
 }
