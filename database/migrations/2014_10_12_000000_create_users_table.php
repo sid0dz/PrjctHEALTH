8976993838d902747371342b87a76e692ce4b65c
nihel-7 <58957635+nihel-7@users.noreply.github.com>
2021-01-08 21:09:39 +0100

    update

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('specialite')->nullable();
             $table->integer('role');
             $table->string('email')->unique();
+            $table->string('accept')->default('1');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
