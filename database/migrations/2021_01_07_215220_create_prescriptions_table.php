79b2af87842f62a88076ac716aa65ab0a14fa136
sid0dz <63064090+sid0dz@users.noreply.github.com>
2021-01-08 13:18:07 +0100

    Revert "Revert "Merge branch 'main' of https://github.com/sid0dz/PrjctHEALTH into main""
    This reverts commit 3bd9d703f98e3e59a85b46fd7efd7f5f358b87b4.

---
+++ database/migrations/2021_01_07_215220_create_prescriptions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrescriptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prescriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->foreignId('medecin_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('patient_id')->constrained('patients')->onDelete('cascade');
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prescriptions');
+    }
+}
