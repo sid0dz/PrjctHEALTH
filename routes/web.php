3bd9d703f98e3e59a85b46fd7efd7f5f358b87b4
sid0dz <63064090+sid0dz@users.noreply.github.com>
2021-01-08 13:03:00 +0100

    Revert "Merge branch 'main' of https://github.com/sid0dz/PrjctHEALTH into main"
    This reverts commit 917b167bac14b0a467625fce589fb2c12c48db27, reversing
    changes made to d8af698485a55f200089ee0bf79eb3bc5bc8a4b1.

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::get('/admin', function () {
-    return view('layouts.admin');
-});
