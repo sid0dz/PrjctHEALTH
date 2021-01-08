c6683f7f8e9cedb19cc45a63d7d76374f94d61ed
sid0dz <63064090+sid0dz@users.noreply.github.com>
2021-01-08 22:03:43 +0100

    Project Health

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
 
 Auth::routes();
 
