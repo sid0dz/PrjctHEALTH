731949b546c8cd8d1cc031529f1a35c48b905819
sid0dz <63064090+sid0dz@users.noreply.github.com>
2021-02-12 17:07:28 +0100

    Update showInfo.blade.php

--- resources/views/dashbord/medecin/showInfo.blade.php
+++ resources/views/dashbord/medecin/showInfo.blade.php
@@ -7,7 +7,7 @@
 	<!-- Earnings (Monthly) Card Example -->
 	<div class="col-xl-3 col-md-6 mb-4">
 		<div class="card border-left-primary shadow h-100 py-2">
-			<div class="card-body">
+			<div class="card-body"><a href="/newRDV/{{$patient->id}}/{{Auth::id()}}/">
 				<div class="row no-gutters align-items-center">
 					<div class="col mr-2">
 						<div class="text-xs font-weight-bold text-primary text-uppercase mb-1"></div>
@@ -16,7 +16,7 @@
 					<div class="col-auto">
 						<i class="fas fa-calendar-alt fa-2x text-gray-300"></i>
 					</div>
-				</div>
+				</div></a>
 			</div>
 		</div>
 	</div>
