8244d0b45364b8889ec1c40c5c8fda4ed72883bf
nihel-7 <58957635+nihel-7@users.noreply.github.com>
2021-02-15 22:58:27 +0100

    sidebar secretaire

--- resources/views/layouts/secretaire.blade.php
+++ resources/views/layouts/secretaire.blade.php
@@ -29,7 +29,18 @@
                 <a class="nav-link collapsed" href="/show_patients" 
                     >
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>renseigner un nouveau patient</span>
+                    <span>Afficher les patients</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" 
+                    >
+                    <i class="fas fa-fw fa-wrench"></i>
+                    <span>Afficher carnet de rendez-vous</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
                     
@@ -37,18 +48,29 @@
             </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-target="#collapseUtilities"
-                    aria-expanded="true" aria-controls="collapseUtilities">                   
-                    <span>Prendre un rendez-vous</span>
-                </a>
-               
-            </li>
+           
 
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                    aria-expanded="true" aria-controls="collapseUtilities">
+                    <i class="fas fa-fw fa-info"></i>
+                    <span>Infos</span>
+                </a>
+                <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
+                    data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Clinique Chifaa</h6>
+                        <a class="collapse-item" >29 Rue Colonel Lotfi</a>
+                        <a class="collapse-item" >Quartier de la liberté.</a>
+                        <a class="collapse-item">Contact@Chifaa.cl</a>
+                        <a class="collapse-item">043-49-50-67</a>
+                    </div>
+                </div>
+            </li>
 
 
         </ul>
