c8d79aa3c6fdbd933779e03e9c32b18ca904ca7a
nardjess <64872078+nardjess@users.noreply.github.com>
2021-02-26 11:09:57 -1200

    Update admin.blade.php

--- resources/views/layouts/Admin/admin.blade.php
+++ resources/views/layouts/Admin/admin.blade.php
@@ -24,7 +24,7 @@
             <!-- Nav Item - Dashboard -->
             <li class="nav-item active">
                 <a class="nav-link" href="{{url('dashboard')}}">
-                    
+                    <i class="fas fa-fw fa-columns"></i>
                     <span>Accueil</span></a>
             </li>
 
@@ -36,14 +36,17 @@
             <li class="nav-item">
                 <a class="nav-link collapsed" href="/show_users" ata-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                
+                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-list" viewBox="0 0 16 16">
+  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+  <path d="M5 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 5 8zm0-2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0 5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-1-5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zM4 8a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm0 2.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0z"/>
+</svg>
                     <span>Afficher les utilisateur</span>
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link collapsed" href="/admconfig" ata-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                
+                 <i class="fas fa-fw fa-wrench"></i>
                     <span>Configurer site</span>
                 </a>
             </li>
